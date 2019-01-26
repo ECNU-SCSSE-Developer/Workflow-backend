d8216f4a0db54cfc96fae43851d7684e4f9abc8c
Fuhy <20292345+Fuhy@users.noreply.github.com>
2019-01-27 00:23:05 +0800

    initialize the project with springboot initializer

---
+++ src/main/java/com/scsse/workflow/WorkflowApplication.java
@@ -0,0 +1,14 @@
+package com.scsse.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowApplication.class, args);
+    }
+
+}
+
