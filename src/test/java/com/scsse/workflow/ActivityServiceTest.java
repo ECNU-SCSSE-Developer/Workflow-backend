b024ea8f70de10c42fc628cc486a7919587db791
scsse-syj <41266430+scsse-syj@users.noreply.github.com>
2019-02-21 21:37:27 +0800

    Activity模块debug

--- src/test/java/com/scsse/workflow/ActivityServiceTest.java
+++ src/test/java/com/scsse/workflow/ActivityServiceTest.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -27,23 +28,33 @@ public class ActivityServiceTest {
     ActivityService activityService;
 
     @Test
+    @Transactional
     public void test() {
+
+        /*Activity newActivity = new Activity("test1", new Date(), "library", "testtesttesttest", new Date());
+        activityService.createActivity(newActivity);
+
+        Activity newActivity2 = new Activity("test2", new Date(), "library", "testtesttesttest", new Date());
+        activityService.createActivity(newActivity2);
+
+        Activity newActivity3 = new Activity("test3", new Date(), "library", "testtesttesttest", new Date());
+        activityService.createActivity(newActivity3);
+
         List<Activity> activities = activityService.findAllActivity();
         for(Activity activity: activities){
             logger.info(activity.toString());
         }
 
-        Activity activity = activityService.findActivityById(2);
+        Activity activity = activityService.findActivityById(11);
         logger.info(activity.toString());
 
-        Activity newActivity = new Activity("test1", new Date(), "library", "testtesttesttest", new Date());
-        activityService.createActivity(newActivity);
 
-        Activity oldActivity = activityService.findActivityById(5);
+        Activity oldActivity = activityService.findActivityById(12);
         oldActivity.setActivityDescription("这是第二届CTF夺旗赛的描述!冲鸭!!!");
-        activityService.updateActivity(oldActivity);
+        activityService.updateActivity(oldActivity);*/
 
-        activityService.deleteActivityById(11);
+        activityService.deleteActivityById(10);
+
     }
 
 }
